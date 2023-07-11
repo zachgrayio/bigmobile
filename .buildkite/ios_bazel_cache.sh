a4dd35bf6f938eaa291ca690d04346ce2f5d8047
Zach Gray <zach.gray@bitrise.io>
2023-07-10 18:10:20 -0700

    wip

--- .buildkite/ios_bazel_cache.sh
+++ .buildkite/ios_bazel_cache.sh
@@ -20,17 +20,18 @@ build --remote_cache=\$BITRISE_CACHE_ENDPOINT
 # needs a token set when spawning the run, or set in pool mgr
 build --remote_header=authorization="Bearer \$BITRISE_CACHE_TOKEN"
 build --remote_header=x-org-id=54743115ea75d779
-build --remote_header=x-org-id=d7188129eb51d1b0 # not really this app id but getting failures without it
 build --experimental_remote_cache_compression
+build --remote_header=x-flare-ac-validation-ttl-sec=360
 #build --experimental_remote_downloader=\$BITRISE_CACHE_ENDPOINT
 #build --remote_downloader_header=authorization="Bearer \$BITRISE_CACHE_TOKEN"
 #build --remote_downloader_header=x-org-id=54743115ea75d779
 
-# build --bes_backend=grpcs://flare-bes.services.bitrise.io:443 
-# build --bes_header=Authorization="Bearer \$BITRISE_CACHE_TOKEN"
-# #build --bes_header=x-step-id=\$BITRISE_STEP_EXECUTION_ID
-# build --invocation_id=\$INV_ID
-# build --remote_header=x-flare-ac-validation-ttl-sec=360
+build --bes_backend=grpcs://flare-bes.services.bitrise.io:443 
+build --bes_header=Authorization="Bearer \$BITRISE_CACHE_TOKEN"
+#build --bes_header=x-step-id=\$BITRISE_STEP_EXECUTION_ID
+build --invocation_id=\$INV_ID
+#build --bes_header=x-app-id=d7188129eb51d1b0
+build --bes_header=x-org-id=54743115ea75d779
 EOF
 
 envsubst < .bitrise.bazelrc.tpl > .bitrise.bazelrc
