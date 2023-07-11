e8d53d3b19610a09736ba033d61ed71f7a952805
Zach Gray <zach.gray@bitrise.io>
2023-07-10 18:16:11 -0700

    wait longer on pings

--- .buildkite/common.sh
+++ .buildkite/common.sh
@@ -4,11 +4,11 @@ set -eo pipefail
 export INV_ID=$(uuidgen)
 echo "invocation ID $INV_ID"
 
-if ping -c 1 -W 1 '10.92.230.152' &> /dev/null
+if ping -c 1 -W 3 '10.92.230.152' &> /dev/null
 then
   # LAS
   export BITRISE_CACHE_ENDPOINT=grpc://10.92.230.152:6666
-elif ping -c 1 -W 1 '10.87.100.50' &> /dev/null
+elif ping -c 1 -W 3 '10.87.100.50' &> /dev/null
 then
   # ATL
   export BITRISE_CACHE_ENDPOINT=grpc://10.87.100.50:6666
