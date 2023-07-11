4b3d7540c6f8fc78e3f10fd0ad1f2ed826c68575
Zach Gray <zach.gray@bitrise.io>
2023-07-10 17:38:21 -0700

    wip

--- .buildkite/ios_bazel_cache.sh
+++ .buildkite/ios_bazel_cache.sh
@@ -44,7 +44,7 @@ mkdir -p $HOME/telegram-provisioning
 cp build-system/appstore-configuration.json $HOME/telegram-configuration/configuration.json
 cp -R build-system/fake-codesigning $HOME/telegram-provisioning/ 
 
-python3 build-system/Make/ImportCertificates.py --path $HOME/telegram-provisioning/fake-codesigning/certs
+#python3 build-system/Make/ImportCertificates.py --path $HOME/telegram-provisioning/fake-codesigning/certs
 
 
 python3 build-system/Make/Make.py \
